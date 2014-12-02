1198e63e71b473f49ed3c95e84d598da3f3ee3b2
Ilia luk <Ilia@domusnetwork.co.il>
2014-12-02 07:11:40 +0200

    Added page cross-testing with Zombie.js

--- meadowlark.js
+++ meadowlark.js
@@ -26,6 +26,18 @@ app.get('/about', function (req, res) {
 	});
 });
 
+app.get('/tours/hood-river', function (req, res) {
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+	res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+	res.render('tours/request-group-rate');
+});
+
 // catch-all handler (middleware)
 app.use(function (req, res, next) {
 	res.status(404);
